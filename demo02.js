8a93aa2138707506da2c6a764204e5544d7824ca
limingru <18813070947@163.com>
2018-02-28 00:27:25 +0800

    add demo02

--- demo02.js
+++ demo02.js
@@ -1,10 +1,29 @@
 
-async function test() {
-    return Promise.resolve('success')
-}
-
-let p = test().then(result => {
-    console.log(result)
+console.log(1)
+let promiseDemo = new Promise((resolve, reject) => {
+    console.log(2)
+    setTimeout(() => {
+        let random = Math.random()
+        if (random >= 0.2) {
+            resolve('success')
+            console.log(3)
+        } else {
+            reject('failed')
+            console.log(3)
+        }   
+    }, 1000)
 })
 
-console.log(p)
+async function test() {
+    console.log(4)
+    let result = await promiseDemo
+    return result
+}
+
+test().then(result => {
+    console.log(5)
+}).catch((result) => {
+    console.log(5)
+})
+
+console.log(6)
